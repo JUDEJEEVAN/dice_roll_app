e4fffe63d58f3da0ce649d710ebc5a7479f9dcc5
Jude <judejeevan.g1@gmail.com>
2024-11-24 21:50:26 +0530

    Class HelloWorld renamed to DiceRollApp and the diceroll app UI created.

--- dice_roll_application/lib/main.dart
+++ dice_roll_application/lib/main.dart
@@ -8,19 +8,49 @@ void addTwoNumbers({required double number1, required double number2}) {
 void main() {
   addTwoNumbers(number2: 4.5, number1: 6.0);
   runApp(MaterialApp(
-    home: HelloWorldApp(),
+    home: DiceRollApp(),
   ));
 }
 
-class HelloWorldApp extends StatelessWidget {
-  const HelloWorldApp({super.key});
+class DiceRollApp extends StatelessWidget {
+  const DiceRollApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('This is the App Bar!'),
-        backgroundColor: Colors.blue.shade400,
+      backgroundColor: Colors.grey.shade200,
+      body: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              'DICE ROLL APP!',
+              style: TextStyle(
+                  fontSize: 40.0,
+                  fontWeight: FontWeight.w800,
+                  color: Colors.black.withOpacity(0.41)),
+            ),
+            Image.asset(
+              'assets/images/dice-1.png',
+              height: 225,
+              width: 225,
+            ),
+            Container(
+              height: 51,
+              width: 298,
+              decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 29, 46, 29),
+                  borderRadius: BorderRadius.circular(12.0)),
+              child: Center(
+                  child: Text(
+                'ROLL DICE',
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              )),
+            ),
+          ],
+        ),
       ),
     );
   }
