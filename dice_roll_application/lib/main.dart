ec223d11a2e1af8d496b3381d07b571ac80b1628
Jude Jeevan Mahendran <judejeevan.g1@gmail.com>
2024-11-27 19:56:19 +0530

    The width of the button is set to 70% of the screen width.

--- dice_roll_application/lib/main.dart
+++ dice_roll_application/lib/main.dart
@@ -39,15 +39,16 @@ class DiceRollApp extends StatelessWidget {
             ),
             Container(
               height: 51,
-              width: 298,
+              width: MediaQuery.of(context).size.width * 0.7,
               decoration: BoxDecoration(
                   color: Color.fromARGB(255, 29, 46, 29),
                   borderRadius: BorderRadius.circular(12.0)),
               child: Center(
-                  child: Text(
-                'ROLL DICE',
-                style: TextStyle(color: Colors.white, fontSize: 20),
-              )),
+                child: Text(
+                  'ROLL DICE',
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+              ),
             ),
           ],
         ),
